2190fc5183a1b6d36a00fc42603de1a3bf0f0e4c
duyenln <duyenln@vietnamblockchain.asia>
2025-05-10 16:19:05 +0700

    fix 404: NOT_FOUND error on vercel

---
+++ src/pages/Employee/index.tsx
@@ -0,0 +1,5 @@
+import EmployeeList from "./EmployeeList";
+import EmployeeCard from "./EmployeeCard";
+import Pagination from "./Pagination";
+export default EmployeeList;
+export { EmployeeCard, Pagination };
